5f0840f492576c41ea49b95c6c0c99ca95531083
Alumno-01 <Alumno-01@DESKTOP-MH79PIJ>
2023-03-06 19:46:03 -0600

    Agregar archivos de proyecto.

---
+++ conexionbd/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace conexionbd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Conectar = new System.Windows.Forms.Button();
+            this.btn_Desconectar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Consulta = new System.Windows.Forms.TextBox();
+            this.dgv_Consulta = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Nombre = new System.Windows.Forms.TextBox();
+            this.txt_Edad = new System.Windows.Forms.TextBox();
+            this.txt_Pais = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Id = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_Actualizar = new System.Windows.Forms.Button();
+            this.btn_Eliminar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_Nomnuevo = new System.Windows.Forms.TextBox();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Consulta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Conectar
+            // 
+            this.btn_Conectar.Location = new System.Drawing.Point(3, 29);
+            this.btn_Conectar.Name = "btn_Conectar";
+            this.btn_Conectar.Size = new System.Drawing.Size(137, 41);
+            this.btn_Conectar.TabIndex = 0;
+            this.btn_Conectar.Text = "CONECTARME";
+            this.btn_Conectar.UseVisualStyleBackColor = true;
+            this.btn_Conectar.Click += new System.EventHandler(this.btn_Conectar_Click);
+            // 
+            // btn_Desconectar
+            // 
+            this.btn_Desconectar.Location = new System.Drawing.Point(3, 88);
+            this.btn_Desconectar.Name = "btn_Desconectar";
+            this.btn_Desconectar.Size = new System.Drawing.Size(137, 40);
+            this.btn_Desconectar.TabIndex = 1;
+            this.btn_Desconectar.Text = "DESCONECTARME";
+            this.btn_Desconectar.UseVisualStyleBackColor = true;
+            this.btn_Desconectar.Click += new System.EventHandler(this.btn_Desconectar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "CONSULTA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PAIS";
+            // 
+            // txt_Consulta
+            // 
+            this.txt_Consulta.Location = new System.Drawing.Point(3, 271);
+            this.txt_Consulta.Name = "txt_Consulta";
+            this.txt_Consulta.Size = new System.Drawing.Size(127, 20);
+            this.txt_Consulta.TabIndex = 4;
+            // 
+            // dgv_Consulta
+            // 
+            this.dgv_Consulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Consulta.Location = new System.Drawing.Point(185, 125);
+            this.dgv_Consulta.Name = "dgv_Consulta";
+            this.dgv_Consulta.Size = new System.Drawing.Size(560, 204);
+            this.dgv_Consulta.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 33);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "AGREGAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 405);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "NOMBRE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(486, 405);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "PAIS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(343, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "EDAD";
+            // 
+            // txt_Nombre
+            // 
+            this.txt_Nombre.Location = new System.Drawing.Point(185, 432);
+            this.txt_Nombre.Name = "txt_Nombre";
+            this.txt_Nombre.Size = new System.Drawing.Size(100, 20);
+            this.txt_Nombre.TabIndex = 10;
+            // 
+            // txt_Edad
+            // 
+            this.txt_Edad.Location = new System.Drawing.Point(316, 432);
+            this.txt_Edad.Name = "txt_Edad";
+            this.txt_Edad.Size = new System.Drawing.Size(100, 20);
+            this.txt_Edad.TabIndex = 11;
+            // 
+            // txt_Pais
+            // 
+            this.txt_Pais.Location = new System.Drawing.Point(453, 432);
+            this.txt_Pais.Name = "txt_Pais";
+            this.txt_Pais.Size = new System.Drawing.Size(100, 20);
+            this.txt_Pais.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(637, 405);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "ID";
+            // 
+            // txt_Id
+            // 
+            this.txt_Id.Location = new System.Drawing.Point(598, 432);
+            this.txt_Id.Name = "txt_Id";
+            this.txt_Id.Size = new System.Drawing.Size(100, 20);
+            this.txt_Id.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(393, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(160, 33);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "SQL SERVER";
+            // 
+            // btn_Actualizar
+            // 
+            this.btn_Actualizar.Location = new System.Drawing.Point(750, 348);
+            this.btn_Actualizar.Name = "btn_Actualizar";
+            this.btn_Actualizar.Size = new System.Drawing.Size(127, 33);
+            this.btn_Actualizar.TabIndex = 16;
+            this.btn_Actualizar.Text = "ACTUALIZAR";
+            this.btn_Actualizar.UseVisualStyleBackColor = true;
+            this.btn_Actualizar.Click += new System.EventHandler(this.btn_Actualizar_Click);
+            // 
+            // btn_Eliminar
+            // 
+            this.btn_Eliminar.Location = new System.Drawing.Point(3, 394);
+            this.btn_Eliminar.Name = "btn_Eliminar";
+            this.btn_Eliminar.Size = new System.Drawing.Size(127, 34);
+            this.btn_Eliminar.TabIndex = 17;
+            this.btn_Eliminar.Text = "ELIMINAR";
+            this.btn_Eliminar.UseVisualStyleBackColor = true;
+            this.btn_Eliminar.Click += new System.EventHandler(this.btn_Eliminar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(745, 400);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 20);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Nombre a Actualizar";
+            // 
+            // txt_Nomnuevo
+            // 
+            this.txt_Nomnuevo.Location = new System.Drawing.Point(750, 431);
+            this.txt_Nomnuevo.Name = "txt_Nomnuevo";
+            this.txt_Nomnuevo.Size = new System.Drawing.Size(133, 20);
+            this.txt_Nomnuevo.TabIndex = 19;
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.ForeColor = System.Drawing.Color.Red;
+            this.btn_Salir.Location = new System.Drawing.Point(757, 24);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(120, 50);
+            this.btn_Salir.TabIndex = 20;
+            this.btn_Salir.Text = "SALIR";
+            this.btn_Salir.UseVisualStyleBackColor = true;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 481);
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.txt_Nomnuevo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btn_Eliminar);
+            this.Controls.Add(this.btn_Actualizar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_Id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_Pais);
+            this.Controls.Add(this.txt_Edad);
+            this.Controls.Add(this.txt_Nombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dgv_Consulta);
+            this.Controls.Add(this.txt_Consulta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Desconectar);
+            this.Controls.Add(this.btn_Conectar);
+            this.Name = "Form1";
+            this.Text = "CONEXION DB";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Consulta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Conectar;
+        private System.Windows.Forms.Button btn_Desconectar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Consulta;
+        private System.Windows.Forms.DataGridView dgv_Consulta;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Nombre;
+        private System.Windows.Forms.TextBox txt_Edad;
+        private System.Windows.Forms.TextBox txt_Pais;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_Id;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_Actualizar;
+        private System.Windows.Forms.Button btn_Eliminar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_Nomnuevo;
+        private System.Windows.Forms.Button btn_Salir;
+    }
+}
+
---
+++ conexionbd/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
+
+namespace conexionbd
+{
+    public partial class Form1 : Form
+    {
+        static string conexionstring = @"server= DESKTOP-MH79PIJ\SQLEXPRESS ; database= master ; integrated security= true ";
+        SqlConnection conexion = new SqlConnection(conexionstring);
+
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap img = new Bitmap(Application.StartupPath + @"\img\ima1.jpg");
+            this.BackgroundImage = img;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void btn_Conectar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            MessageBox.Show("Conexion ala BD:" + conexion.Database + "  ha sido exitoso");
+
+        }
+
+        private void btn_Desconectar_Click(object sender, EventArgs e)
+        {
+            conexion.Close();
+            MessageBox.Show("Se ha desconectado correctamente");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txt_Consulta.Text == "")
+            {
+                string query = "Select * from Personas";
+                SqlCommand comando = new SqlCommand(query, conexion);
+                SqlDataAdapter data = new SqlDataAdapter(comando);
+                DataTable tabla = new DataTable();
+                data.Fill(tabla);
+                dgv_Consulta.DataSource = tabla;
+            }
+            else
+            {
+                string query = "Select * from Personas where Pais = '"+txt_Consulta.Text+"'";
+                SqlCommand comando = new SqlCommand(query, conexion);
+                SqlDataAdapter data = new SqlDataAdapter(comando);
+                DataTable tabla = new DataTable();
+                data.Fill(tabla);
+                dgv_Consulta.DataSource = tabla;
+
+            }
+        }
+
+        public void consulta()
+        {
+            string query = "Select * from Personas";
+            SqlCommand comando = new SqlCommand(query, conexion);
+            SqlDataAdapter data = new SqlDataAdapter(comando);
+            DataTable tabla = new DataTable();
+            data.Fill(tabla);
+            dgv_Consulta.DataSource = tabla;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string cadena = "Insert into personas ([Id],[Nombre],[Edad],[Pais]) values ('"+txt_Id.Text+"','"+txt_Nombre.Text+"','"+txt_Edad.Text+"','"+txt_Pais.Text+"')";
+            SqlCommand comando = new SqlCommand(cadena,conexion);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("La persona:------"+ txt_Nombre.Text+"----Se ha agregado correctamente");
+
+            txt_Id.Text = "";
+            txt_Nombre.Text = "";
+            txt_Edad.Text = "";
+            txt_Pais.Text = "";
+
+
+            consulta();
+        }
+
+        private void btn_Eliminar_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+
+            string cadena = "Delete from Personas where Nombre = '"+txt_Nombre.Text+"'";
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            flag = comando.ExecuteNonQuery();
+
+            if (flag == 1)
+            {
+                MessageBox.Show("Se realizo el delete correctamente");
+            }
+            else
+            {
+                MessageBox.Show("Nose encontro  la persona");
+            }
+            txt_Nombre.Text = "";
+            consulta();
+        }
+
+        private void btn_Actualizar_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+
+            string cadena = "Update Personas set Nombre = '"+txt_Nombre.Text+"', Edad = '"+txt_Edad.Text+"', Pais = '"+txt_Pais.Text+"', Id = '"+txt_Id.Text+"' where Nombre = '"+txt_Nomnuevo.Text+"' ";
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            flag = comando.ExecuteNonQuery();
+
+            if (flag == 1)
+            {
+                MessageBox.Show("Se realizo el cambio correctamente");
+            }
+            else
+            {
+                MessageBox.Show("Nose encontro  la persona");
+            }
+            txt_Nombre.Text = "";
+            txt_Id.Text = "";
+            txt_Nomnuevo.Text = "";
+            txt_Edad.Text = "";
+            txt_Pais.Text = "";
+            consulta();
+
+
+
+
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
